a8dc1797506ffc8a38d12fad06aa449e78f3cc01
mitrajaan92 <mitrajaan92@gmail.com>
2024-12-01 22:29:28 -0500

    day2 static

---
+++ src/main/java/com/binary/day3/AccessModifiers.java
@@ -0,0 +1,9 @@
+package com.binary.day3;
+
+public class AccessModifiers {
+//    1- private: within the declared class
+//    2- public: for all classes and other packages
+//    3- default: within the same package
+//    4- protected: within the same package and subclasses
+
+}
---
+++ src/main/java/com/binary/day3/StaticVSnonStatic.java
@@ -0,0 +1,21 @@
+package com.binary.day3;
+
+public class StaticVSnonStatic {
+//    static attributes: when the value of an attribute is common all the objects of the class, we use
+//    the keyboard static.
+//    static arrtibute can only be changed by the static methods
+//    static attributes belongs to the class itself but not to any specific object. The object cannot change
+//    the static attributes values.
+//    Car obj = new Car();
+//     obj.doors = 4; Wrong
+//    Car.doors = 4; Correct
+//    All the objects of the Car class will have doors =4;
+//    - the purpose is to not let the objects change the value of the attributes
+//    - keyword : static
+//    - static can be applied to attributes and methods
+//    - always public
+//    - it is associated with the class itself, not individual objects
+//    - can be accessed by reference to the class. Car.doors = 4;
+//    non-static: if not static keyword is used then it is not a static members.
+
+}
